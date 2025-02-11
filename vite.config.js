92708132ec3e38fcd5a632b0b0f6300de77ae305
taic-dev <c.t.o.taishi.0530@gmail.com>
2025-02-11 18:41:39 +0900

    fix: css

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@
 import { defineConfig } from 'vite'
 import { glslify } from "vite-plugin-glslify";
 import gltf from 'vite-plugin-gltf';
+import sassGlobImports from "vite-plugin-sass-glob-import";
 
 export default defineConfig({
   root: "src",
   plugins: [
+    sassGlobImports(),
     glslify(),
     gltf({ include: ["**/*.gltf", '**/*.glb'] }),
   ],
